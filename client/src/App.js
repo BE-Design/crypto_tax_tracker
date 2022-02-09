a331a8ef1abde6061e8fed325002fa5e3814cb0f
Eric Davis <davis.25811@gmail.com>
2022-02-09 00:04:38 -0500

    Cleanup dead code in App component

--- client/src/App.js
+++ client/src/App.js
@@ -10,43 +10,6 @@ function  App() {
       <TransactionTable />
     </QueryClientProvider>
   );
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     error: null,
-  //     isLoading: true,
-  //     transactions: []
-  //   };
-  // }
-
-  // componentDidMount() {
-  //   axios('http://127.0.0.1:666/transactions')
-  //     .then(res => {
-  //       this.setState({
-  //         transactions: res.data
-  //       })
-  //     })
-  //     .catch(error => {
-  //       alert('err-or');
-  //       console.error(error);
-  //     })
-  //     .finally(() => {
-  //       this.setState({
-  //         isLoading: false
-  //       })
-  //     });
-  // }
-
-  // render() {
-  //   const { transactions } = this.state;
-  //   return (
-  //     <main>
-  //       <div >
-  //         <TransactionTable transactions={transactions} />
-  //       </div>
-  //     </main>
-  //   );
-  // }
 }
 
 export default App;
