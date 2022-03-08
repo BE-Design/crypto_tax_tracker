aae769d3c7488fde30977302c54d914d8ed07b23
Eric Davis <davis.25811@gmail.com>
2022-03-08 00:44:45 -0500

    Move toast to bottom left

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function  App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TransactionTable />
-      <ToastContainer />
+      <ToastContainer position="bottom-left" />
     </QueryClientProvider>
   );
 }
