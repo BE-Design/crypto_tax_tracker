bcc42d5bb0eb5a231e8d76341f1d9396e4e0ba25
Eric Davis <davis.25811@gmail.com>
2022-03-11 00:38:53 -0500

    Add coin details endpoint to CoinGecko api service

--- client/src/api/services/coingecko.js
+++ client/src/api/services/coingecko.js
@@ -7,21 +7,31 @@ const client = axios.create({
 const CURRENCY = "usd";
 
 class CoinGeckoService {
+
     static async allMarkets() {
         return await client.get(`coins/markets?vs_currency=${CURRENCY}`)
     }
+
     static async price(id) {
         return await client.get(`simple/price?ids=${Array.isArray(id) ? id.join(',') : id}&vs_currencies=${CURRENCY}`)
     }
+
+    static async coinDetails(id) {
+        return await client.get(`coins/${id}`);
+    }
+
     static async search(searchValue) {
         return await client.get(`search?query=${searchValue}`)
     }
+
     static async tickers(id) {
         return await client.get(`coins/${id}/tickers`)
     }
+
     static async ohlcHistory(id, numOfDays) {
         return await client.get(`coins/${id}/ohlc?vs_currency=${CURRENCY}&days=${numOfDays}`)
     }
+
     static async marketHistory(id, numOfDays) {
         return await client.get(`coins/${id}/market_chart?vs_currency=${CURRENCY}&days=${numOfDays}`)
     }
